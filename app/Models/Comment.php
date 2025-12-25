9cbd436ea3f32dbbb78fc10436c29f82b3fea92d
Ali Allouche <ali.hallouch@gmail.com>
2025-12-25 13:57:52 +0100

    feat: complete eloquent relationships in User, Task and Comment models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['content', 'user_id', 'task_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'description', 'status', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
